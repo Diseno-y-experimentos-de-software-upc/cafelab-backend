f217b619c56f32bffcd493faa1225a91e44a4289
Christian1905 <158076892+Christian1905@users.noreply.github.com>
2026-05-11 18:29:25 -0500

    feat: Add delete to grind calibration.

---
+++ src/main/java/com/cafemetrix/cafelab/calibrations/domain/model/commands/DeleteGrindCalibrationCommand.java
@@ -0,0 +1,4 @@
+package com.cafemetrix.cafelab.calibrations.domain.model.commands;
+
+public record DeleteGrindCalibrationCommand(Long calibrationId, Long userId) {
+}
--- src/main/java/com/cafemetrix/cafelab/calibrations/domain/services/GrindCalibrationCommandService.java
+++ src/main/java/com/cafemetrix/cafelab/calibrations/domain/services/GrindCalibrationCommandService.java
@@ -2,6 +2,7 @@ package com.cafemetrix.cafelab.calibrations.domain.services;
 
 import com.cafemetrix.cafelab.calibrations.domain.model.aggregates.GrindCalibration;
 import com.cafemetrix.cafelab.calibrations.domain.model.commands.CreateGrindCalibrationCommand;
+import com.cafemetrix.cafelab.calibrations.domain.model.commands.DeleteGrindCalibrationCommand;
 import com.cafemetrix.cafelab.calibrations.domain.model.commands.UpdateGrindCalibrationCommand;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface GrindCalibrationCommandService {
     Optional<GrindCalibration> handle(CreateGrindCalibrationCommand command);
 
     Optional<GrindCalibration> handle(UpdateGrindCalibrationCommand command);
+
+    boolean handle(DeleteGrindCalibrationCommand command);
 }
